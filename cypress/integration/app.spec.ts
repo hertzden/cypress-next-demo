ee7cf065b78f2ccac85d2fa4eb2b46f2b670f50c
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2021-11-16 22:33:58 +0530

    fix: add snapshot

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,7 +1,13 @@
+import '@percy/cypress';
+
 describe('home page', () => {
     it('should have paragraph tag', () => {
         cy.visit('/')
         cy.get('h1').should('be.visible')
         cy.get('p').should('be.visible')
+        
+    // Take a snapshot for visual diffing
+    cy.percySnapshot();
+
     });
 });
