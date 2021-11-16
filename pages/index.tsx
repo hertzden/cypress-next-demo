e7580380d25f99289ad31681486be389dd85aedc
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2021-11-17 00:24:12 +0530

    fix: breaking ci attempt - 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-         playing around with cypress , next and percy
+         playing around with cypress and percy
         </p>
 
      </main>
