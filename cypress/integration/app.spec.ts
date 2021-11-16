2576360fbd74843c22531e84be5ec8d53ac57183
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2021-11-16 23:38:14 +0530

    fix:second attempt breaking test - 8

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -3,7 +3,8 @@ import '@percy/cypress';
 describe('home page', () => {
     it('should have paragraph tag', () => {
         cy.visit('/')
-      
+        cy.get('h1').should('be.visible')
+        cy.get('p').should('be.visible')
         
     // Take a snapshot for visual diffing
     cy.percySnapshot();
